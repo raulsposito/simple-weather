24440e4c368f15032b4810933fdcd74168f00f17
raulsposito <raulsposito@gmail.com>
2021-01-30 17:35:28 -0300

    add logo, change header size and add fetching data state to app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import logo from './sun2.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      fetchingData: true, 
+      weatherData: {}
+    }
+  }
+
+  render() {
+    const { fetchingData } = this.state 
+    console.log(fetchingData)
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome to Simple Weather!
+          </p>
+
+          fetchingData ?
+          <img src={logo} className="App-logo" alt="logo" />
+          :
+          <p>Forecast is ready!</p>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
